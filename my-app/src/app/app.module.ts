3f8b2e0650a1931dbea656e98a27affa99ddea1d
ODIN\dspecht <daniel.specht@nexolink.net>
2018-12-27 12:00:37 +0100

    Added layout test

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { QuoteGeneratorComponent } from './quote-generator/quote-generator.component';
+import { LayoutTestComponent } from './layout-test/layout-test.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { QuoteGeneratorComponent } from './quote-generator/quote-generator.compo
     TestComponent,
     HeaderComponent,
     NaviComponent,
-    QuoteGeneratorComponent
+    QuoteGeneratorComponent,
+    LayoutTestComponent
   ],
   imports: [
     BrowserModule,
